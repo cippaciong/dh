4b991a4fc14efa497930b87a81cb0bcbbe730180
Tommaso Sardelli <tommaso.sardelli@bottega52.it>
2021-09-13 15:45:44 +0200

    Add a lock on the visitors counter and add logging messages

--- visitors/visitors_counter.py
+++ visitors/visitors_counter.py
@@ -1,5 +1,10 @@
+import logging
+import threading
 from visitors.log_parser import LogParser
 from visitors.visitors_registry import VisitorsRegistry
+
+
+threadLock = threading.Lock()
 
 
 class VisitorsCounter:
@@ -14,5 +19,10 @@ class VisitorsCounter:
         """Evaluates new log entries and increments the visitors counter if
         the visitor's IP is seen for the first time"""
         ip = self.log_parser.get_ip(log_entry)
+        logging.debug(f"Recevied log entry for IP: {ip}")
         if not self.visitors_registry.already_visited(ip):
-            self.visitors += 1
+            with threadLock:
+                self.visitors += 1
+                logging.debug(f"Current number of unique visitors: {self.visitors}")
+        else:
+            logging.debug(f"IP address '{ip}' is already present in the visitors registry")
