b45e3fe6a791f3e336574744776b59dba064c136
Tommaso Sardelli <tommaso.sardelli@bottega52.it>
2021-09-13 13:18:44 +0200

    Rename fixtures in visitor_registry tests and add test for existing visitors

--- tests/test_visitors_registry.py
+++ tests/test_visitors_registry.py
@@ -4,17 +4,21 @@ from visitors.visitors_registry import VisitorsRegistry
 
 
 @pytest.fixture
-def valid_ip():
+def new_visitor():
     return "1.1.1.1"
 
 
 @pytest.fixture
-def invalid_ip():
+def existing_visitor():
     return "0.0.0.0"
 
 
 class TestVisitorsRegistry:
     def test_empty_ips(self):
         visitors_registry = VisitorsRegistry()
-        assert visitors_registry.already_visited(valid_ip) == False
-        assert visitors_registry.already_visited(valid_ip) == True
+        assert visitors_registry.already_visited(new_visitor) == False
+        assert visitors_registry.already_visited(new_visitor) == True
+
+    def test_existing_visitor(self):
+        visitors_registry = VisitorsRegistry({existing_visitor: True})
+        assert visitors_registry.already_visited(existing_visitor) == True
